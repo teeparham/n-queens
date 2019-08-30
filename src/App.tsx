5b5f9283394252a297c8f58cf1b3f8fc4b824b75
Tee Parham <tee@neighborland.com>
2019-08-30 13:09:27 -0600

    Add tachyons, 3 :princess:
    It is ridiculous that you can't modify the <head> of the HTML document with CRApp, to say, link to a stylesheet.

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,14 @@ import './App.css';
 const App: React.FC = () => {
   return (
     <div className="App">
+      <link rel="stylesheet" href="https://unpkg.com/tachyons@4.10.0/css/tachyons.min.css"/>
       <header className="App-header">
         <h1>N Queens</h1>
-        <img src="logo.png" className="App-logo" alt="logo" />
+        <div className="dib">
+          <img src="logo.png" className="App-logo di mh2" alt="logo" />
+          <img src="logo.png" className="App-logo di mh2" alt="logo" />
+          <img src="logo.png" className="App-logo di mh2" alt="logo" />
+        </div>
       </header>
     </div>
   );
