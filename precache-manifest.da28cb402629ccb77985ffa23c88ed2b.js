ac2f89cf242e6d04bd23719bf5c815e78c5c4e0d
Tee Parham <tee@neighborland.com>
2019-09-06 18:11:29 -0600

    Updates

---
+++ precache-manifest.da28cb402629ccb77985ffa23c88ed2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67b72e2d33dfebe46955184a4d992776",
+    "url": "/n-queens/index.html"
+  },
+  {
+    "revision": "3ad7a1d376562f15e876",
+    "url": "/n-queens/static/css/main.4932e64a.chunk.css"
+  },
+  {
+    "revision": "2061fa4cbdc7e1309cf7",
+    "url": "/n-queens/static/js/2.2b8e9831.chunk.js"
+  },
+  {
+    "revision": "3ad7a1d376562f15e876",
+    "url": "/n-queens/static/js/main.77423bf6.chunk.js"
+  },
+  {
+    "revision": "90a4b4c9da8c0fd4b918",
+    "url": "/n-queens/static/js/runtime~main.cade7a26.js"
+  }
+]);
